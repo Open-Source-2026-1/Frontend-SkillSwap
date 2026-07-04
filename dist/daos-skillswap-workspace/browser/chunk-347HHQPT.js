a893d76cebab6e60121d7a5245c28294dd134dbd
Victor Alberca <vitito199844@gmail.com>
2026-07-04 11:50:27 -0500

    Eliminar archivos ignorados del repositorio

--- dist/daos-skillswap-workspace/browser/chunk-347HHQPT.js
+++
@@ -1,1 +0,0 @@
-import{Sb as p,Za as n,vb as o,wb as a}from"./chunk-5N5ZSK7Y.js";import"./chunk-2NFLSA4Y.js";var s=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-page-not-found"]],decls:2,vars:0,template:function(e,d){e&1&&(o(0,"p"),p(1,"page-not-found works!"),a())},encapsulation:2})};export{s as PageNotFound};
